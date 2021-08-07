f4ab39bca33676c888d361472543cb2e8c7b9138
erick <muriithierick3758@gmail.com>
2021-08-07 09:19:59 +0300

    changed default setting

---
+++ resources/views/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block
+            text-center">
+<h1 class="sm:text-white text-5xl uppercase font-bold
+text-shadow-md pb-14">
+Do you want to Become a developer ?
+
+</h1>
+
+    <a href="/blog"
+    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-boldtext-xl 
+    uppercase">
+    Read More
+</a>
+            </div>
+        </div>
+    </div>
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b
+    border-gray-200">
+    <div>
+        <img src="#" width ="700 "alt="">
+    </div>
+    <div class="m-auto sm:auto text-left w-4/5 block">
+        <h2 class="text-4xl font-extrabold text-gray-600">
+            Struggling to be a better Developer
+
+        </h2>
+        <p  class="py-8 text-gray-500 text-xl">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.         </p>
+
+            <p class="font-extrabold text-gray-600 text-s pb-9">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quae praesentium in amet vitae error asperiores! Voluptatem.
+
+            </p>
+            <a href="/blog"
+            class="uppercase bg-blue-500 text-gray-100 text-s
+            font-extrabold py-3 rounded-3xl">
+        Find out more</a>
+    </div>
+    <div class="text-center p-15 bg-black text-white">
+<h2 class="text-2xl pb-5 text-l">
+    I'm an expert in ..
+</h2>
+<span class="font-extrabold block text-4xl py-1">
+    Ux Design
+
+</span>
+<span class="font-extrabold block text-4xl py-1">
+Project management
+
+</span>
+<span class="font-extrabold block text-4xl py-1">
+   Digital Strategy
+
+</span>
+<span class="font-extrabold block text-4xl py-1">
+Backend Developer
+
+</span>
+    </div>
+
+    </div>
+    <div class="text-center py-15">
+        <span class="uppercase text-s text-gray-400">
+            Blog
+        </span>
+        <h2 class="text-4xl font-bold py-10">
+            Recent Posts
+        </h2>
+        <p class="m-auto w-4/5 text-gray-500">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda vitae illo quae iste, enim sit nisi doloremque sed cum ratione quis, asperiores obcaecati velit at odit consequuntur adipisci. Eligendi, corporis.
+
+        </p>
+
+    </div>
+
+@endsection
